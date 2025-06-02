8cad7cc9ff779fa376e6d5a78a4cdfd25f84288b
xzy.sung <xzy.sung@gmail.com>
2025-06-02 03:27:33 +0000

    javascript abrir e fechar o menu completo

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-function main() {
-  console.log("Hello, World!");
+const botaoAbrir = document.querySelector(".header-mobile > button");
+const botaoFechar = document.querySelector(".header-mobile nav button");
+
+const body = document.querySelector("body");
+const nav = document.querySelector(".header-mobile nav");
+
+botaoAbrir.addEventListener("click", abrirMenu)
+botaoFechar.addEventListener("click", fecharMenu)
+
+function abrirMenu(){
+  body.classList.add("escurecer");
+  nav.classList.add("abrir");
 }
 
-main();
+function fecharMenu(){
+  body.classList.remove("escurecer");
+  nav.classList.remove("abrir");
+}
